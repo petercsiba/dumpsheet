743eaf3d40948020dc8a36d9cd1f8f175891ba8a
Peter Csiba <petherz@gmail.com>
2023-07-12 12:07:41 -0700

    first pass in customizing to Voxana, button color (fail), add description to the Task List, update favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent-1': '#333',
+        // 'accent-1': '#333',
+        'accent-1': '#981161',  // Tailwind default purple is a855f7
+        'purple': '#a21063',  // gradient [350e47, dd1173]
       },
     },
   },
