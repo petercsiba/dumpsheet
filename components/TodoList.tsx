a0a725e9be9d168161ac285d86633ec64da7b700
Peter Csiba <petherz@gmail.com>
2023-07-12 12:07:41 -0700

    first pass in customizing to Voxana, button color (fail), add description to the Task List, update favicon

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -55,7 +55,7 @@ export default function TodoList({ session }: { session: Session }) {
 
   return (
     <div className="w-full">
-      <h1 className="mb-12">Todo List.</h1>
+      <h1 className="mb-12">Task List</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault()
@@ -73,7 +73,7 @@ export default function TodoList({ session }: { session: Session }) {
             setNewTaskText(e.target.value)
           }}
         />
-        <button className="btn-black" type="submit">
+        <button className="btn-purple" type="submit">
           Add
         </button>
       </form>
@@ -92,6 +92,7 @@ export default function TodoList({ session }: { session: Session }) {
 const Todo = ({ todo, onDelete }: { todo: Todos; onDelete: () => void }) => {
   const supabase = useSupabaseClient<Database>()
   const [isCompleted, setIsCompleted] = useState(todo.is_complete)
+  const [showDescription, setShowDescription] = useState(false);
 
   const toggle = async () => {
     try {
@@ -110,38 +111,52 @@ const Todo = ({ todo, onDelete }: { todo: Todos; onDelete: () => void }) => {
   }
 
   return (
-    <li className="w-full block cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out">
-      <div className="flex items-center px-4 py-4 sm:px-6">
-        <div className="min-w-0 flex-1 flex items-center">
-          <div className="text-sm leading-5 font-medium truncate">{todo.task}</div>
-        </div>
-        <div>
-          <input
-            className="cursor-pointer"
-            onChange={(e) => toggle()}
-            type="checkbox"
-            checked={isCompleted ? true : false}
-          />
-        </div>
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            e.stopPropagation()
-            onDelete()
-          }}
-          className="w-4 h-4 ml-2 border-2 hover:border-black rounded"
+      <li className="w-full block">
+        <div
+            className="flex items-center px-4 py-4 sm:px-6 cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out"
+            onClick={() => setShowDescription(!showDescription)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray">
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
+          <div className="min-w-0 flex-1 flex flex-col">
+            <div className="text-sm leading-5 font-medium truncate">
+                {todo.task}
+            </div>
+          </div>
+          <div>
+            <input
+                className="cursor-pointer"
+                onChange={(e) => toggle()}
+                type="checkbox"
+                checked={isCompleted ? true : false}
             />
-          </svg>
-        </button>
-      </div>
-    </li>
-  )
+          </div>
+          <button
+              onClick={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                onDelete()
+              }}
+              className="w-4 h-4 ml-2 border-2 hover:border-black rounded"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="gray">
+              <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        {showDescription &&
+            <div className="flex items-center px-4 py-4 sm:px-6">
+              <div className="min-w-0 flex-1 flex flex-col">
+                <div className="min-w-250 text-sm py-4">
+                  Hey Katka, Great running into you! Really admire your endeavor to build Katka AI, sparks a unique vibe. Being from Slovakia must add a lot to your unique perspective. Count me in your corner, should you ever need anything!
+                </div>
+              </div>
+            </div>
+        }
+      </li>
+  );
 }
 
 const Alert = ({ text }: { text: string }) => (
