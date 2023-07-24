8b81509267e5161a794c758fdc8b5b0fa52e5257
Peter Csiba <petherz@gmail.com>
2023-07-23 19:47:59 -0700

    change is_new_account for needs_email for cases when account exists, but email ain't set

--- sam_app/upload_voice/app.py
+++ sam_app/upload_voice/app.py
@@ -109,7 +109,7 @@ def lambda_handler(event, context):
         # Works for API Gateway
         request_id = event["requestContext"]["requestId"]
         # We only want to collect the email address if not already associated with this IP address.
-        response["is_new_account"] = is_new
+        response["needs_email"] = is_new or acc.get_email() is None
         response["account_id"] = str(acc.id)  # maybe we should have a UUIDEncoder
 
         models.BaseDataEntry.insert(
