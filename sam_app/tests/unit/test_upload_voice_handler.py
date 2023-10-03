494ff3fb1a422fe23e4af4abf4ef8e938c98d5e2
Peter Csiba <petherz@gmail.com>
2023-10-03 14:42:22 -0700

    fix test imports

--- sam_app/tests/unit/test_upload_voice_handler.py
+++ sam_app/tests/unit/test_upload_voice_handler.py
@@ -6,12 +6,13 @@ from uuid import UUID
 import pytest
 
 from common.config import ENV
-from database.account import ACCOUNT_STATE_MERGED, Account
+from database.account import Account
 from database.client import (
     POSTGRES_LOGIN_URL_FROM_ENV,
     connect_to_postgres_i_will_call_disconnect_i_promise,
     disconnect_from_postgres_as_i_promised,
 )
+from database.constants import ACCOUNT_STATE_MERGED
 from database.data_entry import STATE_UPLOAD_INTENT
 from database.models import BaseDataEntry, BaseOnboarding
 
