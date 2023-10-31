32c42113aebe7effc3cd31b3b76cbbeed641074d
Peter Csiba <peter@voxana.ai>
2023-10-31 10:26:31 -0700

    buttons are rounded-full, containers are rounded-lg

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -39,7 +39,7 @@ const Banner = () => {
                 */}
             </div>
             {hubspot_status && (
-                <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center text-center border border-black rounded-full bg-white px-6 py-1">
+                <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center text-center border border-black lg bg-white px-6 py-1">
                     <div className="block text-black font-semibold text-center">
                         {hubspot_status === 'success' && <span className="text-green-700">Successfully connected your HubSpot!</span>}
                         {hubspot_status !== 'success' && <span className="text-red-600">Failed connecting your HubSpot!</span>}
@@ -50,7 +50,7 @@ const Banner = () => {
                 </div>
             )}
             {!hubspot_status && (
-                <div className="w-[16rem] absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center text-center border border-black rounded-full bg-white px-6 py-1">
+                <div className="w-[16rem] absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center text-center border border-black rounded-lg bg-white px-6 py-1">
                     <div className="block text-black font-semibold text-center">
                         We are in Private Beta
                     </div>
