ba6d618d218332d2d09f61367ff48c935ff5d881
Peter Csiba <petherz@gmail.com>
2023-11-05 14:21:55 -0800

    Add Taylor Swift, revamp onboarding copy, refactor CollectEmailProcessingInfo, mvp persist user session

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -42,7 +42,7 @@ const Banner = () => {
                 <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center text-center border border-black lg bg-white px-6 py-1">
                     <div className="block text-black font-semibold text-center">
                         {hubspot_status === 'success' && <span className="text-green-700">Successfully connected your HubSpot!</span>}
-                        {hubspot_status !== 'success' && <span className="text-red-600">Failed connecting your HubSpot!</span>}
+                        {hubspot_status !== 'success' && <span className="text-red-500">Failed connecting your HubSpot!</span>}
                     </div>
                     <div className="block text-center">
                         {hubspot_status === 'success' ? 'Lets do your first recording' : 'Please reach out to support@voxana.ai'}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        {/* Start of HubSpot Embed Code */}
+        {
+            /* Start of HubSpot Embed Code
+            TODO(P1, compliance): We probably need to disclose this tracking */
+        }
         <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/40211602.js"></script>
       </body>
     </Html>
