c6cb760774e178c34ac998af976ea08aa55a2028
Peter Csiba <petherz@gmail.com>
2023-11-07 22:27:05 -0800

    [components] Make the current progress bar step to breath

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -10,19 +10,19 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ currentStep }) => {
             <div className="h-0.5 bg-gray-300 mb-4" style={{ width: '92%' }}></div> {/* Horizontal line */}
             <div className="w-full flex justify-between items-center">
                 <div className="flex flex-col items-center">
-                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 1 ? 'bg-orange-500' : currentStep > 1 ? 'bg-green-500' : 'bg-gray-300'}`}>
+                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 1 ? 'animate-pulse bg-orange-500' : currentStep > 1 ? 'bg-green-500' : 'bg-gray-300'}`}>
                         1
                     </div>
                     <span className="text-xs mt-1 m-1 text-center">Record a Voice Note</span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 2 ? 'bg-orange-500' : currentStep > 2 ? 'bg-green-500' : 'bg-gray-300'}`}>
+                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 2 ? 'animate-pulse bg-orange-500' : currentStep > 2 ? 'bg-green-500' : 'bg-gray-300'}`}>
                         2
                     </div>
                     <span className="text-xs mt-1 mx-1 text-center">Enter Your Email</span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 3 ? 'bg-orange-500' : currentStep > 3 ? 'bg-green-500' : 'bg-gray-300'}`}>
+                    <div className={`w-8 h-8 rounded-full flex items-center justify-center text-white font-bold ${currentStep === 3 ? 'animate-pulse bg-orange-500' : currentStep > 3 ? 'bg-green-500' : 'bg-gray-300'}`}>
                         3
                     </div>
                     <span className="text-xs mt-1 mx-1 text-center">Receive Organized Recap</span>
