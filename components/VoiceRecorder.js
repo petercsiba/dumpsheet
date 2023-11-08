def116c4159bc09297d6ce74af97b69a3e2fc813
Peter Csiba <peter@voxana.ai>
2023-11-07 22:53:17 -0800

    [style] Make it nicer

--- components/VoiceRecorder.js
+++ components/VoiceRecorder.js
@@ -200,15 +200,14 @@ const RegisterEmailState = ({accountId, onRegistrationSuccess}) => {
 const SuccessState = ({comesFromDemo, userEmailAddress, onRecordAgain}) => {
     return (
         <>
-            <HeadingText text={comesFromDemo ? "Demo Complete!" : "Congrats, All Done Here!"}/>
+            <HeadingText text={comesFromDemo ? "Demo Complete!" : "Congrats - All Done Here!"}/>
             <div className="pl-4">
-                <span className="font-bold text-base">Now, you</span>
+                <span className="font-bold text-base">Now, you can</span>
                 <ul className="list-disc list-inside text-">
-                    <li className="mt-1">Go check email(s) from assistant@voxana.ai
-                        <br /> &nbsp; &nbsp; (should arrive within a few minutes)
+                    <li className="mt-1">Review email(s) from assistant@voxana.ai
+                        <br /> &nbsp; &nbsp; &nbsp; (arriving within a few minutes)
                     </li>
-                    <li className="mt-1">Send responses / follow ups to your contacts</li>
-                    <li className="mt-1">Fill in your Voxana spreadsheet</li>
+                    <li className="mt-1">Send follow-ups to your contacts</li>
                 </ul>
             </div>
 
@@ -220,13 +219,13 @@ const SuccessState = ({comesFromDemo, userEmailAddress, onRecordAgain}) => {
                     <ProgressBar currentStep={3}/>
                     <div className="h-0.5 bg-gray-300 my-4 justify-items-end" style={{ width: '92%' }}></div> {/* Horizontal line */}
                     {/*<div className="pt-8 pb-2"><b>More encounters on your mind?</b></div>*/}
-                    {/*<div><SelectButton label={"Record Another One"} onClick={onRecordAgain}/></div>*/}
+                    {/*<div><SelectButton label={"Record Another Memo"} onClick={onRecordAgain}/></div>*/}
                     <div className="flex justify-end">
                         <button
                             onClick={onRecordAgain}
                             className="px-4 py-1 mr-0 text-black border border-black rounded-full font-semibold text-base tracking-tighter bg-white hover:bg-gray-300"
                         >
-                            Record Another One
+                            Record Another Memo
                         </button>
                     </div>
                 </>
